882f3f07686cf872c4b8da02d411614aac1b6744
Chris Denneen <cdenneen@gmail.com>
2011-08-09 11:02:11 -0400

    modified

--- home/Welcome_files/Welcome.js
+++ home/Welcome_files/Welcome.js
@@ -1,7 +1,7 @@
-// Created by iWeb 3.0.4 local-build-20110808
+// Created by iWeb 3.0.4 local-build-20110809
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke(),reflection_0:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00}),stroke_0:new IWEmptyStroke()});registry.applyEffects();}
 function hostedOnDM()
 {return false;}
 function onPageLoad()
